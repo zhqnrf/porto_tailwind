727af4a325aa46962cf15c1d276cf680785f91ec
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2023-12-27 11:57:06 +0700

    publish

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        dark: '#0f172a'
+      }
+    },
+  },
+  plugins: [],
+}
